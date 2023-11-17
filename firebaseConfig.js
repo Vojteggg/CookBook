220a2b5184c46c53280e108edac724b32293a321
Wojciech <wlos@st.swps.edu.pl>
2023-11-17 17:45:23 +0100

    ciekwe czy dziala

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARLoQuHnEX5jYkTg0g0pgC4iSKAsFULzY",
+  authDomain: "cookbook-25975.firebaseapp.com",
+  projectId: "cookbook-25975",
+  storageBucket: "cookbook-25975.appspot.com",
+  messagingSenderId: "927835319272",
+  appId: "1:927835319272:web:ff31d9da63a910905c125d",
+  measurementId: "G-S1QLTVHGMM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export {db};
